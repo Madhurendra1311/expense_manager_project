263f119ba3f662f7c59da37d40f6925c360971d8
soundaryamecse <soundaryamecse@gmail.com>
2020-10-11 17:39:38 +0530

    Nav Bar Added

---
+++ expense_manager/src/Component/Ledger.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Ledger extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Ledger</h1>
+            </div>
+        )
+    }
+}
