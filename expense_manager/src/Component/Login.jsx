1109eb453e21f9f67658438026024052545536a4
soundaryamecse <soundaryamecse@gmail.com>
2020-10-11 16:30:12 +0530

    NavbarAdded

---
+++ expense_manager/src/Component/Login.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { userLogin } from "../Redux/actions/loginAction";
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    if(email.length > 0 && password.length > 0){
+      let payload = {
+        "email" : email,
+        "password" : password
+    }
+    this.props.loginRequest(payload);
+    }else{
+      alert("Fill all Details")
+    }
+  };
+  render() {
+    const { email, password } = this.state;
+    const { isAuth, isLoading, error, userData} = this.props;
+    console.log(isAuth, isLoading,error, userData);
+    if (!isAuth)
+      return (
+        <>
+          <form onSubmit={this.onSubmit}>
+            <div>email:
+              <input
+                onChange={this.handleChange}
+                name="email"
+                placeholder="email"
+                type="text"
+                value={email}
+              />
+            </div>
+            <div>Password:
+              <input
+                onChange={this.handleChange}
+                name="password"
+                placeholder="password"
+                type="password"
+                value={password}
+              />
+            </div>
+            <div>
+              <input value="submit" type="submit" />
+            </div>
+          </form>
+          {isLoading && "...loading"}          
+          {
+              !isAuth && error  && <div>Invalid Credentials pls try Again !!!</div>
+          }
+        </>
+      );
+    else {
+      return ( 
+       <Redirect to="/dashboard"/>        
+      )
+
+    }
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuth: state.login.isAuth,
+  error: state.login.error,
+  isLoading: state.login.isLoading,
+  profile: state.login.profile,
+  userData : state.login.userData
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loginRequest: (payload) => dispatch(userLogin(payload))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
+
+
---
+++ expense_manager/src/Routes/Routes.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import Dashboard from "../Component/Dashboard";
+import Register from "../Component/Register"
+import Login from "../Component/Login";
+import Ledger from "../Component/Ledger"
+import Home from "../Component/Home"
+
+
+
+function Routes(props) {
+    return (
+      <>
+        <Switch>       
+
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/login" render={(props) => <Login {...props} />} /> */}
+          <Route path="/dashboard" render={() => <Dashboard />} />
+          <Route path="/register" render={() => <Register />} />
+          <Route path="/login" render={()=><Login {...props}/>} />
+          <Route path="/ledger" render={() => <Ledger />} />
+          <Route>
+            <div>Error 404 </div>
+            <Link to="/">GO BACK HOME</Link>
+          </Route>
+        </Switch>
+      </>
+    );
+  }
+  
+  export { Routes };
