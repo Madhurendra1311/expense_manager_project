ecc9121abb41561e877c5d25ea4398ef4635d6fa
soundaryamecse <soundaryamecse@gmail.com>
2020-10-11 17:32:26 +0530

    Revert "NavbarAdded"
    This reverts commit 1109eb453e21f9f67658438026024052545536a4.

--- expense_manager/src/App.js
+++ expense_manager/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-// import Navbar from "./Component/Navbar"
-import { Routes } from "./Routes/Routes";
-// import Footer from "./Component/Footer"
-
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{height:"1000px"}}>    
-      {/* <Navbar /> */}
-      <Routes />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
